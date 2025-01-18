8f82367961d0019619fa65a9bb889c3396cc17c7
norlan <norlanantoni18mail.com>
2025-01-18 13:34:28 -0600

    no se porque no funciona

--- obj/Release/net8.0/CLINICA.AssemblyInfo.cs
+++ obj/Release/net8.0/CLINICA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CLINICA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7306efef8703d35c9425ab7a31be17e879a1d4a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c587151953e630b01fe24eef830f584872cd958b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CLINICA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CLINICA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
